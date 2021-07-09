8fd1caf96b28af480134704fd6828616a2032aef
kalecgos13 <kalecgos13@gmail.com>
2021-07-09 16:47:46 +0200

    added localization and tooltip to BrokenEgg

--- src/main/java/com/kalec/Items/BrokenEggItem.java
+++ src/main/java/com/kalec/Items/BrokenEggItem.java
@@ -1,10 +1,28 @@
 package com.kalec.Items;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BrokenEggItem extends Item {
     public BrokenEggItem() {
         super((new Item.Properties()).maxStackSize(16).group(ItemGroup.MISC));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("empty_row"));
+        tooltip.add(new TranslationTextComponent("item.eggs.tooltip.broken_egg"));
     }
 }
